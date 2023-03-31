8455ae82b796efa3ccf11ffda23e2fa775b6cc91
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-03-31 23:22:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # VARIABLE
-token = os.getenv("5851945481:AAHyxkGMsxi0bOipQSJmcZTHv6tbabFu_UU")
-sudo = os.getenv("1715348447")
+token = os.getenv("BOT_TOKEN", "5851945481:AAHyxkGMsxi0bOipQSJmcZTHv6tbabFu_UU")
+sudo = os.getenv(" SUDO", "1715348447")
 
 
 # JAWABAN BOT
-kenapa = ["gatau", "ntahlah", "tanya orang lain aja", "yntkts", "karna because tidak pernah never", "bodoamat", "apasiii wibu gaboleh nanya", "karna lu wibu", "karna aku mencintaimu", "karna kamu jelek", "pikir sendiri", "tanya google aja"]
+kenapa = ["gatau", "ntahlah", "tanya orang lain aja", "kamu nanya?", "karna because tidak pernah never", "bodoamat", "apasiii wibu gaboleh nanya", "karna lu wibu", "karna aku mencintaimu", "karna kamu jelek", "pikir sendiri", "tanya google aja"]
 
 kapan = ["mungkin sekarang", "1 jam lagi", "2 jam lagi", "8 jam lagi", "hari ini", "mungkin besok", "besok lusa", "sebulan lagi", "setahun lagi", "sewindu lagi", "tahun lalu", "tahun 1945 17 agustus", "tahun 2070", "diem deh gausah nanya", "apasi wibu", "pikir sendiri", "seabad lagi", "5000 SM", "minggu ini", "tahun ini"]
 
 apakah = ["betul", "tidak", "iya", "ga", "kepo deh", "diem deh gausah nanya mulu", "bisa jadi si", "kurang tau", "bodo ah", "hmm", "ga mungkin", "kayanya iya", "mungkin aja", "mustahil", "pikir sendiri", "sudah pasti", "yoiii"]
 
+Siapa = ["Kim jong-un", "Jokowi", "kamu nanya?", "Puan maharani", "Siapa aja boleh", "Coba tanya kibo", "Hmmmmm kasih tau gak yach",]
+
+Dimana = ["Planet pluto", "Coba tanya kibo, karena dia dukun", " dihatiku", "dimana dimana anak kambing saya.... Lanjutin lagunya"]
